aaf996fdb458dd62896b7830ab044a2fc431fa8a
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-10-03 20:34:33 +0530

    Done with logic of subset sum count

--- DP_subsetSumCount.cpp
+++ DP_subsetSumCount.cpp
@@ -8,9 +8,9 @@ int main() {
 	for(i=0;i<n;cin>>a[i],++i);
 	int dp[n+1][sum+1];
 	memset(dp,0,sizeof(dp));
-	for(dp[0][0]=0,i=1;i<=n;++i){
+	for(dp[0][0]=1,i=1;i<=n;++i){
 	    for(j=1;j<=sum;++j){
-	        if(a[i-1]<=j)dp[i][j]=((dp[i][j-a[i-1]])+(dp[i-1][j]));
+	        if(a[i-1]<=j)dp[i][j]=((dp[i-1][j-a[i-1]])+(dp[i-1][j]));
 	        else dp[i][j]=dp[i-1][j];
 	    }
 	}
